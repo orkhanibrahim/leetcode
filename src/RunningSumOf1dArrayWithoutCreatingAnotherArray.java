1fff9b72210070d3b67bdbc5e2fb2627d730e511
ibrahimot <orxan.ibrahimov@kapitalbank.az>
2022-12-22 00:32:43 +0400

    optimized using shorthand operator

--- src/RunningSumOf1dArrayWithoutCreatingAnotherArray.java
+++ src/RunningSumOf1dArrayWithoutCreatingAnotherArray.java
@@ -6,7 +6,7 @@ public class RunningSumOf1dArrayWithoutCreatingAnotherArray {
     }
     public static int [] runningSum(int[] nums) {
         for(int i = 1; i<nums.length; i++) {
-            nums[i] = nums[i] + nums[i-1];
+            nums[i] += nums[i-1];
         }
         return nums;
     }
